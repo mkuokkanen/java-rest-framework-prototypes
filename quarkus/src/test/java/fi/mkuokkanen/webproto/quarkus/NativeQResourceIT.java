0a3b0c08b38d506b5d88d49ea7a027a5ebd4b66e
Mikko Kuokkanen <mikko.kuokkanen@gmail.com>
2019-10-08 21:47:16 +0300

    Quarkus proto

---
+++ quarkus/src/test/java/fi/mkuokkanen/webproto/quarkus/NativeQResourceIT.java
@@ -0,0 +1,9 @@
+package fi.mkuokkanen.webproto.quarkus;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeQResourceIT extends QResourceTest {
+
+    // Execute the same tests but in native mode.
+}
